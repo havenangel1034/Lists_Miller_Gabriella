fab0690db3c5418a2ef3193bd745b33ac922e04c
Gabby <gabbie10112005@gmail.com>
2025-02-03 08:07:57 -0600

    Initial commit

---
+++ Lists_Miller_Gabriella/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lists_Miller_Gabriella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
